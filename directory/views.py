083b36cfd4554d322b15b6e247b771bea70631a3
Vasco Patricio <vasco.patricio.pessoal@gmail.com>
2010-04-23 14:02:05 +0100

    - A lista de empresas no site principal ja filtra por aprovadas. Numero total ainda aparece como total de todas.

--- directory/views.py
+++ directory/views.py
@@ -13,8 +13,23 @@ def submit(request):
     if request.method == 'POST':
 	form = CompanyForm(request.POST)
 	if(form.is_valid()):
+	    newCompany = Company(
+		name = form.cleaned_data['name'],
+		logo = form.cleaned_data['logo'],
+		description = form.cleaned_data['description'],
+		website = form.cleaned_data['website'],
+		email = form.cleaned_data['email'],
+		address = form.cleaned_data['address'],
+		phone = form.cleaned_data['phone'],
+		year = form.cleaned_data['year'],
+		map_lat = form.cleaned_data['map_lat'],
+		map_lon = form.cleaned_data['map_lon'],
+		tags = form.cleaned_data['tags'],	    
+	    )
+	    newCompany.save()
+    
 	    return HttpResponseRedirect('/../')
     else:
 	form = CompanyForm()
-
+    
     return render(request,'submit.html',{'form':form})
