2c3d0a382e93501662fdf612bd40416ceaaeba8c
Sergio Santos <me@sergiosantos.info>
2010-04-26 21:38:32 +0100

    Started using the reverse function in redirects

--- directory/views.py
+++ directory/views.py
@@ -4,6 +4,7 @@ from directory.utils import *
 from models import *
 from forms import *
 from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 def index(request):
     companies = Company.objects.order_by('?')
@@ -11,13 +12,11 @@ def index(request):
     
 def submit(request):
     if request.method == 'POST':
-	form = CompanyForm(request.POST)
-	
-	if(form.is_valid()):
-	    newCompany = form.save()
-    
-	    return HttpResponseRedirect('/../')
+        form = CompanyForm(request.POST)
+        if form.is_valid():
+           newCompany = form.save()
+           return HttpResponseRedirect(reverse('index'))
     else:
-	form = CompanyForm()
+        form = CompanyForm()
     
     return render(request,'submit.html',{'form':form})
