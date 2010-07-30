605d5aa6b03d30de1c7e88360887cfcc3182db4f
Sergio Santos <me@sergiosantos.info>
2010-07-30 18:11:31 +0100

    Url to company on map.

--- directory/views.py
+++ directory/views.py
@@ -9,8 +9,19 @@ from django.http import HttpResponseRedirect
 
 def index(request):
     success = 'success' in request.GET and request.GET['success']
+    
+    if 'company' in request.GET:
+        company_id = request.GET['company']
+        try:
+            Company.objects.get(id=company_id, approved=True)
+        except:
+            company_id = None
+    else:
+        company_id = None
+            
+    
     companies = Company.objects.filter(approved='True').order_by('?')
-    return render(request,'index.html',{'companies':companies, 'success':success})
+    return render(request,'index.html',{'companies':companies, 'success':success, 'company_id':company_id})
     
 def submit(request):
     if request.method == 'POST':
--- urls.py
+++ urls.py
@@ -12,9 +12,6 @@ feeds = {
 
 
 urlpatterns = patterns('',
-    # (r'^yoursite/', include('yoursite.foo.urls')),
-	# (r'^admin/doc/', include('django.contrib.admindocs.urls')), 
-	# And add 'django.contrib.admindocs' to INSTALLED_APPS
     
     (r'^admin/(.*)', admin.site.root),
     (r'^media/(.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
